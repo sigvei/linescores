7e9d5a46cf7a7f41c2309694d52934a572096c39
Sigve Indregard <sigve.indregard@gmail.com>
2012-10-01 10:38:13 +0200

    Initial commit.
    Empty Rails + Rspec repository.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Linescores::Application.config.secret_token = 'cfb7debdd073c352a24449d81ac1ca380cfc9427f0386a605b7062629e8ffeebde6cdd835e7901e1209f692cd8643daf4fad0a31276054f5164a695a03e8254a'
