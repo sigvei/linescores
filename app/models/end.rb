513d896cb892c5cb9334f68e71e5e89474d0c98c
Sigve Indregard <sigve.indregard@gmail.com>
2012-10-02 11:07:34 +0200

    Match and End integration and views

--- app/models/end.rb
+++ app/models/end.rb
@@ -4,4 +4,28 @@ class End < ActiveRecord::Base
   belongs_to :match
 
   acts_as_list :scope => :match
+
+  def won?
+    result > 0
+  end
+
+  def blanked?
+    result == 0
+  end
+
+  def lost?
+    result < 0
+  end
+
+  def our_hammer?
+    if first?
+      match.first_hammer?
+    else
+      higher_item.lost? or (higher_item.blanked? and higher_item.our_hammer?)
+    end
+  end
+
+  def stole?
+    won? and (not our_hammer?)
+  end
 end
--- app/models/match.rb
+++ app/models/match.rb
@@ -6,7 +6,7 @@ class Match < ActiveRecord::Base
 
   has_many :ends, :order => "position"
 
-  def score
+  def score(nend=nil)
     if ends.size == 0
       [nil, nil]
     else
@@ -21,11 +21,50 @@ class Match < ActiveRecord::Base
     end
   end
 
-  def our_score
-    score[0]
+  def our_score(nend=nil)
+    if nend # get score for a certain end
+      if ends[nend - 1]      # if that end exists at all
+        e = ends[nend - 1]   # get the end
+        if e.won?            # we won the end
+          e.result.abs
+        elsif e.blanked? && e.our_hammer? # we blanked it
+          0
+        else                 # we lost or thet blanked
+          nil
+        end
+      else # end does not exist
+        nil
+      end
+    else # get the score for the entire match
+      score[0]
+    end
   end
   
-  def opponent_score
-    score[1]
+  def opponent_score(nend=nil)
+    if nend # get score for a certain end
+      if ends[nend - 1]      # if that end exists at all
+        e = ends[nend - 1]   # get the end
+        if e.lost?           # we lost the end
+          e.result.abs
+        elsif e.blanked? && (! e.our_hammer?) # they blanked it
+          0
+        else                 # we won or blanked
+          nil
+        end
+      else # end does not exist
+        nil
+      end
+    else # get the score for the entire match
+      score[1]
+    end
+  end
+
+  def first_hammer?
+    # TODO: Implement
+    true
+  end
+
+  def their_first_hammer?
+    not first_hammer?
   end
 end
